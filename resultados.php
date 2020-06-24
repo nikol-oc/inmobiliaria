73b4503da464615598ddf4292b812e4eceffb692
Nikol Lopez <nikol.oc.96@gmail.com>
2020-06-23 23:16:34 -0500

    version1

---
+++ resultados.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row">
+          <h2 style="color:#009688">Viviendas encontradas:</h2>  
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-title text-center">
+                        HOTEL DE GLACE
+                        </div>
+                    </div>
+                    <div class="card-body p-0">
+                        <img class="img-fluid "src="img/hotel1.jpg" alt="" style="height:250px">
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-title text-center">
+                        THE ATLANTIS
+                        </div>
+                    </div>
+                    <div class="card-body p-0">
+                        <img class="img-fluid "src="img/hotel2.jpg" alt=""  style="height:250px">
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-title text-center">
+                        GALLERY HOTEL ART.
+                        </div>
+                    </div>
+                    <div class="card-body p-0" >
+                        <img class="img-fluid "src="img/hotel3.jpg" alt=""  style="height:250px">
+                    </div>
+                </div>
+            </div>
+          
+        </div>
+        <hr>
+        <div class="row">
+            [<a href="busqueda.html"> Buscar otra vivienda</a>]
+        </div>
+    </div>
+</body>
+</html>
